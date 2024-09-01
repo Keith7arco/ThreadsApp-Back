ab8cbb37cdfb96e57a4db72ec757a6b95d3ce0c7
Keith Tarco <keithtarco07@gmail.com>
2024-09-01 10:28:56 -0500

    V1.0.0

---
+++ src/comments/dto/create-comment.dto.ts
@@ -0,0 +1,13 @@
+import { IsNotEmpty, IsString } from "class-validator";
+
+export class CreateCommentDto {
+  @IsNotEmpty()
+  @IsString()
+  text:String;
+
+  @IsNotEmpty()
+  @IsString()
+  userId: String;
+
+  parentId:null | string;
+}
